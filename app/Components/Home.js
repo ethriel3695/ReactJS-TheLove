56b492822ed5529aefa5a438cfd57e0c2b5ba10a
GOVERNET\reubenellis <rellis@governet.net>
2017-06-27 16:49:38 -0600

    Added styling for images

--- app/Components/Home.js
+++ app/Components/Home.js
@@ -3,17 +3,22 @@ var Link = require('react-router-dom').Link;
 //var images = require('Images/images');
 import * as imageGroup from './Images.js';
 
+//USE THIS LINK TO CREATE A CAROUSEL
+//https://codepen.io/nopr/pen/rfBJx
+
 class Home extends React.Component {
     render() {
         return (
             <div className='home-container'>
-                <h1>A Glimpse at Love!</h1>
-                <div className='carouselImages'>
-                    <img src={imageGroup.firstpic} alt='' />
-                </div>
-                <div className='carouselImages'>
-                    <img src={imageGroup.secondpic} alt='' />
-                </div>
+                <h1>A Glimpse of Love!</h1>
+                <ul>
+                <li>
+                    <img className='carouselImages' src={imageGroup.firstpic} alt='' />
+                </li>
+                <li>
+                    <img className='carouselImages'  src={imageGroup.secondpic} alt='' />
+                </li>
+                </ul>
             </div>
         )
     }
