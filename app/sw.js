7755e51b45ef41ea03717c7a63bd7aca1aae1b64
GOVERNET\reubenellis <rellis@governet.net>
2017-07-13 08:38:27 -0600

    Making web app progressive

---
+++ app/sw.js
@@ -0,0 +1,7 @@
+//https://codelabs.developers.google.com/codelabs/migrate-to-progressive-web-apps/#3
+//FOR PROGRESSIVE WEB APP
+
+/** An empty service worker! */
+self.addEventListener('fetch', function(event) {
+  /** An empty fetch handler! */
+});
